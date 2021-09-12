6505b2067956b5a0d5524eb7572b4a0f584fa8b7
Masite-1 <63239895+Masite-1@users.noreply.github.com>
2021-09-12 23:32:35 +0800

    Create TestProgrem.cs

---
+++ TestProgrem.cs
@@ -0,0 +1,8 @@
+using System;
+ namespace A{
+     public static class Program {
+         public static void main(){
+         Console.WriteLine("hello world");
+         }               
+     }
+ }
