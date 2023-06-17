76b6c75f86db3bebc3a463902b63152862ffb186
Wuxu <63239895+Masite-1@users.noreply.github.com>
2023-06-17 22:17:19 +0800

    delete something (#1)

--- TestProgrem.cs
+++ TestProgrem.cs
@@ -5,7 +5,7 @@ namespace HelloWorldApplication
     {
         static void Main(string[] args)
         {
-            /* 我的第一个 C# 程序*/
+          
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
